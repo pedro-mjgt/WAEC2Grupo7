51c8d7b3f6377b350a937775ff560b64d69c31d1
pedro-mjgt <pedro.mjgt@gmail.com>
2024-04-27 11:49:41 -0500

    inicio

---
+++ src/main/java/pe/edu/cibertec/WAEC2Grupo7/Waec2Grupo7Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2Grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2Grupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2Grupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/WAEC2Grupo7/Waec2Grupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2Grupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2Grupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
